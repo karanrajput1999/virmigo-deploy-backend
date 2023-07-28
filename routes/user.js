2bb12c270296de7521dc66b287445db654c75ad6
Karan <rajputkaran441@gmail.com>
2023-07-28 22:22:03 +0530

    user profile working dynamically

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,20 @@ const express = require("express");
 const userRouter = express.Router();
 const User = require("../Models/user");
 
+userRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(400)
+      .send("Something went wrong! while fetching user profile", error);
+  }
+});
+
 userRouter.post("/:id", async (req, res) => {
   // id of user, we want to send to friend request to
   const { id } = req.params;
