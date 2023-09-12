2459213297785500c7ebc6f84bbb1166dede3626
Karan <rajputkaran441@gmail.com>
2023-09-13 00:02:34 +0530

    some changes

--- app.js
+++ app.js
@@ -16,14 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: "http://127.0.0.1:5173",
     origin: "https://virmigo.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
 );
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.setHeader("Access-Control-Allow-Origin", "https://virmigo.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
