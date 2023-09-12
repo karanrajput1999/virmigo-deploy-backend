0f02ae19c89c7543e7e742e771b4d2701540f249
Karan <rajputkaran441@gmail.com>
2023-09-12 17:49:28 +0530

    some changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    // origin: "http://127.0.0.1:5173",
     origin: "virmigo.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
