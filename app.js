5edf07386cd9465c60b59be0a67b57271f40af56
Karan <rajputkaran441@gmail.com>
2023-09-13 12:16:06 +0530

    some changes

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://virmigo.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   next();
 });
 app.use(cookieParser());
